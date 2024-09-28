2fa7c33ced26c3d4c3091e0589d603aa0d526e43
Sumbal Naz <Codeq209@gmail.com>
2024-09-28 18:59:35 +0500

    Add files via upload

---
+++ Step2-Components/src/app/Components/page.tsx
@@ -0,0 +1,9 @@
+const Components = () => {
+    let a = "Do You Know About Components??"
+    return (
+        <div>
+            <h3>{a}</h3>
+        </div>
+    )
+}
+export default Components
---
+++ Step2-Components/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Components from "./Components/page";
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Hello World</h3>
+      <Components/>
+    </div>
+  )
+}

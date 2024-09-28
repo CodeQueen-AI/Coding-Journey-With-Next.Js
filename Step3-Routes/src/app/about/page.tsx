dbafa6fbfb9bb0d1ab84407caa916ea785b4e2c9
Sumbal Naz <Codeq209@gmail.com>
2024-09-28 20:48:39 +0500

    Add files via upload

---
+++ Step3-Routes/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>I am Enjoying Learning Next.js</div>
+    )
+}
+export default About
---
+++ Step3-Routes/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = ()  => {
+  return (
+    <div>Happy Coding!</div>
+  )
+}
+
+export default HomePage
